fcdc18bb3b99772183efee6b4184cc5dd2ee9f68
Riccardo <riccardo.mattivi@gmail.com>
2025-07-11 21:58:59 +0200

    package

--- lint.sh
+++ lint.sh
@@ -2,11 +2,18 @@
 
 set -e
 
-# Formatters
-black codetok tests examples
-isort codetok tests examples
+MODE=${1:-format}
 
-# Linters
+if [ "$MODE" = "check" ]; then
+  echo "Running in CHECK mode (CI style)"
+  black --check codetok tests examples
+  isort --check-only codetok tests examples
+else
+  echo "Running in FORMAT mode (auto-fix)"
+  black codetok tests examples
+  isort codetok tests examples
+fi
+
 flake8 codetok tests examples
 mypy codetok tests examples
 bandit -r codetok
